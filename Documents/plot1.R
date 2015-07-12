2901dd9962f363008dc88591255023c253a7c06d
krishna2001 <krisrao20@hotmail.com>
2015-07-12 00:11:46 -0500

    add plot1

---
+++ Documents/plot1.R
@@ -0,0 +1,7 @@
+data<-data.frame()
+data<-read.table("household_power_consumption.txt", sep=";", dec=".", header=TRUE, stringsAsFactors=FALSE)
+smalldata<-subset(data, data$Date=="1/2/2007" | data$Date=="2/2/2007")
+gapdata<-as.numeric(smalldata$Global_active_power)
+hist(gapdata, col="red",main="Global Active Power", xlab="Global Active Power (kilowatts)" )
+dev.copy(png, file="plot1.png", width=480, height=480)
+dev.off()
